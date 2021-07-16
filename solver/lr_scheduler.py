77494fe63f6fd7d2e4f3a299cffd29e716e550a5
omid <omiderfanmanesh.dev@gmail.com>
2021-07-16 08:15:09 +0200

    add multistepLR

--- solver/lr_scheduler.py
+++ solver/lr_scheduler.py
@@ -1,8 +1,8 @@
 from ignite.contrib.handlers.param_scheduler import LRScheduler
-from torch.optim.lr_scheduler import StepLR
+from torch.optim.lr_scheduler import MultiStepLR
 
 
 def scheduler(optimizer):
-    step_scheduler = StepLR(optimizer, step_size=100, gamma=0.1)
+    step_scheduler = MultiStepLR(optimizer, milestones=[60, 120, 160], gamma=0.2)
     lr_scheduler = LRScheduler(step_scheduler)
     return lr_scheduler
