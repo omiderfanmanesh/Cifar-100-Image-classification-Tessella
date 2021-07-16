c728a98820244b1f4e87ac4ae19b66405c173141
omid <omiderfanmanesh.dev@gmail.com>
2021-07-16 17:20:46 +0200

    add randaugmenation add GradualWarmupScheduler

--- solver/lr_scheduler.py
+++ solver/lr_scheduler.py
@@ -1,13 +1,14 @@
+from ignite.contrib.handlers.param_scheduler import LRScheduler
 from torch.optim.lr_scheduler import MultiStepLR
-from warmup_scheduler import GradualWarmupScheduler
 
 
 def scheduler(optimizer):
     step_scheduler = MultiStepLR(optimizer, milestones=[60, 120, 160])
-    lr_scheduler = GradualWarmupScheduler(
-        optimizer,
-        multiplier=2,
-        total_epoch=3,
-        after_scheduler=step_scheduler
-    )
+    # lr_scheduler = GradualWarmupScheduler(
+    #     optimizer,
+    #     multiplier=2,
+    #     total_epoch=3,
+    #     after_scheduler=step_scheduler
+    # )
+    lr_scheduler = LRScheduler(step_scheduler)
     return lr_scheduler
